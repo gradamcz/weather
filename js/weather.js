ebb1621bb146e6bad96e025a3f2bec59a2bf05ad
George <gratech@workmail.com>
2025-10-15 13:05:34 -0400

    Initial upload with subfolders

---
+++ js/weather.js
@@ -0,0 +1,127 @@
+// External JavaScript for weather site
+// George A updated 2025
+
+// Swap image function
+function swapImage(imageSrc) {
+    var imageBox = document.getElementById("change-image");
+    imageBox.src = imageSrc;
+}
+
+// Preload images for faster swapping
+const swapImageURLs = [
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-CONUS-GEOCOLOR-625x375.gif",
+  "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/10/625x375.jpg",
+  "https://s.w-x.co/staticmaps/wu/wxtype/county_loc/pie/animate.png",
+  "https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/dynamic/wbbh/nbc2_watervapor_web.jpg?crop=1xw:1xh;center,top&resize=900:*",
+  "https://www.weather.gov/images/tbw/graphicast/Today.png",
+  "https://www.weather.gov/images/tbw/graphicast/Tonight.png",
+  "https://www.weather.gov/images/tbw/graphicast/Tomorrow.png",
+  "https://www.wpc.ncep.noaa.gov/qpf/p120i.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/allfcsts_loop_ndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/91fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/92fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/93fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/94fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/95fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/96fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/98fndfd.gif",
+  "https://www.wpc.ncep.noaa.gov/basicwx/99fndfd.gif"
+];
+swapImageURLs.forEach(url => {
+  const img = new Image();
+  img.src = url;
+});
+
+// Service worker registration
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw - ORIG.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err));
+}
+
+// DateDiv display logic
+function showDateDivsByMonth() {
+    var dateDivs = document.querySelectorAll(".DateDiv");
+    var currentMonth = new Date().getMonth() + 1;
+    dateDivs.forEach(function(dateDiv) {
+        var dateRange = dateDiv.querySelector(".DateRange").textContent;
+        var [dtFrom, dtTo] = dateRange.split(" to ").map(Number);
+        if (currentMonth >= dtFrom && currentMonth <= dtTo) {
+            dateDiv.style.display = "block";
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", showDateDivsByMonth);
+
+// Modal notice logic
+function setCookie(cname, cvalue, exdays) {
+    var expires = "";
+    var date = new Date();
+    date.setDate(date.getDate() + 7);
+    var midnight = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 23, 59, 59);
+    expires = "; expires=" + midnight.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function myFunction2() {
+    var x = document.getElementById("ModalNotice");
+    var user = getCookie("Modalshown");
+    if (user == "") {
+        x.className = "show";
+    }
+    setTimeout(function() {
+        x.className = x.className.replace("show", "");
+    }, 19500);
+}
+window.onload = myFunction2;
+function myFunction1() {
+    var x = document.getElementById("ModalNotice");
+    x.className = x.className.replace("show", "");
+    setCookie("Modalshown", "yes", 1);
+}
+
+// PWA installation logic
+let deferredPrompt;
+const installButton = document.getElementById('install-button-main');
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  if (installButton) {
+      installButton.style.display = 'block';
+      installButton.addEventListener('click', () => {
+        installButton.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then(choiceResult => {
+          console.log(choiceResult.outcome === 'accepted' ? 
+            'User accepted install' : 'User dismissed install');
+          deferredPrompt = null;
+        });
+      });
+  }
+});
+window.addEventListener("appinstalled", () => {
+  console.log("PWA installed successfully!");
+  if (installButton) installButton.style.display = "none";
+  deferredPrompt = null;
+});
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.matchMedia("(display-mode: standalone)").matches || 
+      window.navigator.standalone) {
+    console.log("App is running in standalone mode.");
+    if (installButton) installButton.style.display = "none";
+  }
+});
